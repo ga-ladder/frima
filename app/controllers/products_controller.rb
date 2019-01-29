a9c2ca58b832f8027907c36d6f3297548ea16687
Genta <8581682+ga-ladder@users.noreply.github.com>
2019-01-29 23:31:40 +0900

    fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,7 @@ class ProductsController < ApplicationController
         format.json { render template: "products/create.html.erb" }
       end
     else
+      flash[:notice] = 'not yet'
       render :new
     end
   end
