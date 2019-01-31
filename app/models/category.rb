744601a3ccee5b4059a66e225286159dea4a4a88
Genta <8581682+ga-ladder@users.noreply.github.com>
2019-01-31 23:10:59 +0900

    edit show page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  # has_many :subcategories, class_name: "Category",
-  #                         foreign_key: "parent_id",
-  #                         dependent: :destroy
-  # belongs_to :parent, class_name: "Category",
-  #                    optional: true
+  has_one :product
 
   def self.root_set
     where("parent_id = ?", 0)
