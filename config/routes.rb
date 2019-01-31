4fedb3aaf95767ddaf3cdb1b1ea5c402009ea500
Genta <8581682+ga-ladder@users.noreply.github.com>
2019-02-01 02:30:25 +0900

    edit session/new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   resources :products
