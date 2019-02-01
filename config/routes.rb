ea1efc55559de44796bf5e009ae160b1584f0110
Genta <8581682+ga-ladder@users.noreply.github.com>
2019-02-01 11:24:14 +0900

    add login_way page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
       }
-  resources :users
+  resources :users do
+    collection do
+      get 'login_way'
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
