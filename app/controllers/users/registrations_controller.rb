e56689546e78f2a2bbbc342e7aac39b69ff6a898
Genta <8581682+ga-ladder@users.noreply.github.com>
2019-02-10 05:18:31 +0900

    add registration page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -38,25 +38,83 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  def phone
+  end
+
+  def resident
+    @resident = Resident.new
+  end
+
+  def resident_registration
+    @resident = Resident.new(resident_params)
+    if @resident.save
+      redirect_to customer_registration_url
+    else
+      render :action => "resident"
+    end
+  end
+
+  def customer
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    api_customer = Payjp::Customer.create(
+      description: current_user.id,
+      email: current_user.email
+    )
+    customer = Customer.create(customer_id: api_customer.id, user_id: current_user.id)
+  end
+
+  def customer_registration
+    customer_id = current_user.customer.customer_id
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    # カード情報を登録
+    customer = Payjp::Customer.retrieve(customer_id)
+    begin
+      customer.cards.create(card: params['payjpToken'])
+    rescue Payjp::InvalidRequestError => e
+      logger.error e
+      redirect_to :action => "customer", alert: "カード情報が正しくないか，すでに登録されています"
+      return
+    end
+    redirect_to complete_url
+  end
+
+  def complete
+  end
+
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :nickname])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :nickname])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    phone_registration_url
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def resident_params
+    params.require(:resident).permit(
+      :real_first_name,
+      :real_last_name,
+      :kana_first_name,
+      :kana_last_name,
+      :birthday,
+      :zip_code,
+      :prefecture,
+      :city_name,
+      :house_number,
+      :building_name,
+      :phone_number).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,22 @@ Rails.application.routes.draw do
         sessions: 'users/sessions',
         registrations: 'users/registrations'
       }
+  devise_scope :user do
+    get 'phone_registration' => 'users/registrations#phone'
+    post 'resident_registration' => 'users/registrations#resident_registration'
+    get 'resident_registration' => 'users/registrations#resident'
+    post 'customer_registration' => 'users/registrations#customer_registration'
+    get 'customer_registration' => 'users/registrations#customer'
+    get 'complete' => 'users/registrations#complete'
+  end
   resources :users do
     collection do
-      get 'login_way'
+      get 'sign_up_way'
+      get 'logout'
     end
   end
+  resources :residents
+  resources :customers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
