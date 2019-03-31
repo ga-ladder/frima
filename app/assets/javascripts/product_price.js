62452b28af34a37fcbd430b1526489fc11016e4f
Genta <8581682+ga-ladder@users.noreply.github.com>
2019-04-01 00:38:26 +0900

    add tax,...

--- app/assets/javascripts/product_price.js
+++ app/assets/javascripts/product_price.js
@@ -1,13 +1,16 @@
 $(function(){
   if(window.location.href.indexOf("products") > -1){
-
-    var inputPrice = $("#product_price");
-    inputPrice.on("change", function(){
-      var price = inputPrice.val();
+    if ($("#product_price").val() != "") {
+      var price = $("#product_price").val();
       var commission = Math.ceil(price / 10);
       $(".form__price__commission")[0].innerText = commission;
       $(".form__price__commission")[1].innerText = price - commission;
-    })
-
+    }
+    var inputPrice = $("#product_price");
+    inputPrice.on("keyup", function(){
+      var commission = Math.ceil($(this).val() / 10);
+      $(".form__price__commission")[0].innerText = commission;
+      $(".form__price__commission")[1].innerText = $(this).val() - commission;
+    });
   }
 })
